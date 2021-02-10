4411aad9a0531646a540931de8c2c18019831e61
Daniel <ahribori@gmail.com>
2021-02-10 19:58:07 +0900

    스크롤 해야하는 스토리 추가

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,38 +1,54 @@
-import React from 'react';
+import React from "react"
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/react/types-6-0';
+import { Story, Meta } from "@storybook/react/types-6-0"
 
-import { Button, ButtonProps } from '../components/Button/Button';
+import { Button, ButtonProps } from "../components/Button/Button"
 
 export default {
-  title: 'Example/Button',
+  title: "Example/Button",
   component: Button,
   argTypes: {
-    backgroundColor: { control: 'color' },
-  },
-} as Meta;
+    backgroundColor: { control: "color" }
+  }
+} as Meta
 
-const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Primary = Template.bind({})
 Primary.args = {
   primary: true,
-  label: 'Button',
-};
+  label: "Button"
+}
 
-export const Secondary = Template.bind({});
+export const Secondary = Template.bind({})
 Secondary.args = {
-  label: 'Button',
-};
+  label: "Button"
+}
 
-export const Large = Template.bind({});
+export const Large = Template.bind({})
 Large.args = {
-  size: 'large',
-  label: 'Button',
-};
+  size: "large",
+  label: "Button"
+}
 
-export const Small = Template.bind({});
+export const Small = Template.bind({})
 Small.args = {
-  size: 'small',
-  label: 'Button',
-};
+  size: "small",
+  label: "Button"
+}
+
+const Template2: Story<ButtonProps> = (args) => (
+  <>
+    {Array.from(new Array(30)).map((_, index) => (
+      <div key={`button_${index}`}>
+        <Button {...args} />
+      </div>
+    ))}
+  </>
+)
+
+export const Loop = Template2.bind({})
+Loop.args = {
+  size: "small",
+  label: "Loop"
+}
